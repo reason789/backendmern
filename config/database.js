05240de12fef7629e15d69a0c24066162f3a1e2f
reason789 <rahadul134@gmail.com>
2023-01-17 08:45:17 +0600

    first

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(
+      "mongodb+srv://sixPackProgrammer:sixPackProgrammer@cluster0.0jijw.mongodb.net/Ecommerce?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    )
+    .then((data) => {
+      console.log(`Mongodb connected with server: ${data.connection.host}`);
+    });
+};
+
+module.exports = connectDatabase;
